5457a75076bd422432f039db813d74516f90f643
Christine McClure <camcclure@gmail.com>
2022-05-06 14:47:44 -0500

    initial commit (use between computers)

---
+++ hello-react/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+
+
+ReactDOM.render(
+  <ul>
+    <li>thing 1</li>
+ </ul>,
+  document.getElementById('root')
+);
+
+
